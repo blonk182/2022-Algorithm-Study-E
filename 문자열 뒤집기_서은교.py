6f4e00c8321d4e514d2147f667c47875b5d9cc1b
EunKyoSeo <>
2022-05-30 08:42:13 +0000

    commit

---
+++ 문자열 뒤집기_서은교.py
@@ -0,0 +1,19 @@
+s = input()
+
+now = int(s[0])
+
+result = [0] * 2
+
+result[now] = 1
+
+for i in s:
+    
+    num = int(i)
+    if now == num:
+        continue
+  
+    else:
+        result[num] += 1
+        now = num
+
+print(min(result[0], result[1]))
